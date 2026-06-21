429db366baee11de2e69ff164d6f0f68ce319d55
Yefri Gavarrete <130026945+YefriGavarrete@users.noreply.github.com>
2026-06-21 14:27:57 -0600

    Metodos Eliminar + Actualizar + Listar terminados

--- Sistema_Crud_Grupo3/Gestion_Empleados/controladores/EmpleadosController.php
+++ Sistema_Crud_Grupo3/Gestion_Empleados/controladores/EmpleadosController.php
@@ -1,10 +1,9 @@
-<? 
+<?php 
     require_once('./config/conexion.php');
 
 
-    $mensaje = "";
-
 class EmpleadosController{
+    
     public static function listar(){
         global $conexion;
         $stmt = $conexion->prepare("SELECT * FROM empleados ORDER BY id DESC");
@@ -16,7 +15,56 @@ class EmpleadosController{
 
     public static function GuardarEmpleados($nombre, $puesto, $salario, $fecha_ingreso){
         global $conexion;
-        $conexion->prepare("INSERT INTO");
+        $stmt = $conexion->prepare("INSERT INTO empleados (nombre, puesto, salario, fecha_registro ) VALUES (:nombre, :puesto, :salario, :fecha_registro)");
+        $stmt->bindParam(":nombre", $nombre);
+        $stmt->bindParam(":puesto", $puesto);
+        $stmt->bindParam(":salario", $salario);
+        $stmt->bindParam(":fecha_ingreso", $fecha_ingreso);
+
+        if($stmt->execute()){
+            header("Location: index.php?msg=guardado");
+
+        }
+    }
+
+
+
+    public static function ObtenerPorId($id){
+        global $conexion;
+        $stmt = $conexion->prepare("SELECT * FROM empleados WHERE id= :id");
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+
+    }
+
+    public static function EditarEmpleado($id, $nombre, $puesto, $salario, $fecha_ingreso){
+        global $conexion;
+        $stmt = $conexion->prepare("UPDATE empleados  SET nombre = :nombre, puesto = :puesto, salario = :salario, fecha_ingreso = :fecha_ingreso WHERE id= :id");
+        $stmt->execute();
+        $stmt->bindParam(":id", $id);
+        $stmt->bindParam(":nombre", $nombre);
+        $stmt->bindParam(":puesto", $puesto);
+        $stmt->bindParam(":salario", $salario);
+        $stmt->bindParam(":fecha_ingreso", $fecha_ingreso);
+
+        if($stmt->execute()){
+            header("Location: index.php?msg=guardado");
+
+        }
+
+    }
+
+    public static function EliminarEmpleado($id){
+        global $conexion;
+        $stmt = $conexion->prepare("DELETE FROM empleados WHERE id = :id");
+        $stmt->bindParam(":id", $id);
+        if ($stmt->execute()){
+            header("Location: index.php?message=eliminado");
+        }
+       
 
     }
 
